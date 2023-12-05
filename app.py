a0d53c54a5dc5b7f2c06caf2ca325a66e31dbcea
smit0101 <smitkalkani9099@gmail.com>
2023-12-05 15:16:55 -0500

    Frontend creatend smit kalkani

---
+++ app.py
@@ -0,0 +1,54 @@
+
+import streamlit as st
+import requests
+
+# Define API endpoints
+BASE_URL = "http://0.0.0.0:8080"  
+ADD_TASK_ENDPOINT = "/addtask"
+DELETE_ALL_TASK_ENDPOINT = "/deletealltask"
+GET_ALL_TASKS_ENDPOINT = "/tasks"
+
+# Streamlit app
+def main():
+    st.title("Todo List App")
+
+    # Input field for adding a new task
+    new_task = st.text_input("Add a new task:")
+
+    # Button to add a new task
+    if st.button("Add Task"):
+        add_task(new_task)
+
+    # Display the current tasks
+    display_tasks()
+
+# Function to add a new task
+def add_task(task):
+    try:
+        response = requests.post(BASE_URL + ADD_TASK_ENDPOINT, json={"task": task})
+        response.raise_for_status()
+        st.success(f"Task '{task}' added successfully!")
+    except requests.exceptions.RequestException as e:
+        st.error(f"Error adding task: {e}")
+
+# Function to display the current tasks
+def display_tasks():
+    try:
+        response = requests.get(BASE_URL + GET_ALL_TASKS_ENDPOINT)
+        response.raise_for_status()
+        tasks = response.json()
+
+        # Display tasks
+        if tasks:
+            st.subheader("Current Tasks:")
+            for task in tasks:
+                st.write(f"- {task.get('task')}")
+        else:
+            st.info("No tasks available.")
+
+    except requests.exceptions.RequestException as e:
+        st.error(f"Error fetching tasks: {e}")
+
+if __name__ == "__main__":
+    main()
+
